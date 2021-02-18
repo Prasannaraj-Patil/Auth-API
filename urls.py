91e8cb3a7fb759312f955fc0f579bd9311a576e9
Prasannaraj-Patil <55536882+Prasannaraj-Patil@users.noreply.github.com>
2021-02-18 12:34:43 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,28 @@
-from django.conf.urls import url, include
-from rest_framework import routers
-from api.views import UserViewSet,UserProfileViewSet
+"""restapi URL Configuration
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'profile',UserProfileViewSet)
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.urls import path
+from django.contrib import admin
+
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^auth/', include('rest_auth.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('api.urls')),
+    # path('api/reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('api/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('api/reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('api/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
